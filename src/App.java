05fec6c17f31864027d0281c67442ee06f3d2b76
THEspager <danilagon10@gmail.com>
2022-03-26 15:40:10 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) {
+        new CreateWindow("Discchat", 800, 600, true, "chats", true, "join srever");
+    }
+}
